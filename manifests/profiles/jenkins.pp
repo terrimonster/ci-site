b0e8e3515a8e97d00db90178219b4d0fbdf18241
Terri Haber <terrimonster@gmail.com>
2014-01-09 14:22:12 -0800

    enabling repos that contain java-devel rpm

--- manifests/profiles/jenkins.pp
+++ manifests/profiles/jenkins.pp
@@ -1,5 +1,13 @@
 class ci::profiles::jenkins {
   include ::jenkins
+  yumrepo { 'C6.2-extras':
+    enabled  => '1',
+    before => Class['java'],
+  }
+  yumrepo { 'C6.2-updates':
+    enabled  => '1',
+    before => Class['java'],
+  }
   class { 'java':
     distribution => 'jdk',
     version      => 'latest',
